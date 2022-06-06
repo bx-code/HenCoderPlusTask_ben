c32dda304931942ec535a777153482973e443d15
白潇 <xiao.bai@bkjk.com>
2022-06-06 10:12:17 +0800

    第7节kotlin 基础作业源码

---
+++ MyApplication2/base/src/main/java/com/example/core/BaseApplication.kt
@@ -0,0 +1,19 @@
+package com.example.core
+
+import android.app.Application
+import android.content.Context
+
+class BaseApplication : Application(){
+
+    companion object{
+        private lateinit var currentApplication : Context
+        fun currentApplication(): Context{
+            return currentApplication
+        }
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        currentApplication=this
+    }
+}
