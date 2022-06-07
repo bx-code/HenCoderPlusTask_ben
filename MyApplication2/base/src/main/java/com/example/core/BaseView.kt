31b7c0b65ee2a9d02d3bffc291d5982532731236
白潇 <xiao.bai@BKJK.com>
2022-06-07 20:39:29 +0800

    remove all file

--- MyApplication2/base/src/main/java/com/example/core/BaseView.kt
+++
@@ -1,6 +0,0 @@
-package com.example.core
-
-interface  BaseView<T>  {
-     fun  getPresenter() : T
-
-}
--- MyApplication2/base/src/main/java/com/example/core/http/EntityCallback.kt
+++
@@ -1,9 +0,0 @@
-package com.example.core.http
-
-import androidx.annotation.NonNull
-import androidx.annotation.Nullable
-
-interface EntityCallback<T> {
-     fun onSuccess (@NonNull t: T)
-     fun onFailure (@Nullable message: String)
-}
